386cd549a60d11ef452c37a78bf6046549f5d31a
Nhicko Jerome Garachico <nhickojerome.garac@hcl.com>
2025-09-12 17:46:52 +0800

    [autocomment] added logs

--- src/index.ts
+++ src/index.ts
@@ -28,24 +28,44 @@ async function summarizeFile(input: string, filePath?: string): Promise<any> {
 }
 
 if (llmData) {
-  const jiraKey = llmData.jiraKey;
+  const { branchName, jiraKey } = llmData;
+  console.log('--- Initializing Git to Jira ---');
+  console.log(`Running on branch: ${branchName}`);
+  console.log(`Will comment on Jira issue: ${jiraKey}`);
+
   if (jiraKey) {
     try {
       // Get jira issue
       const jiraIssue = await getIssue(jiraKey);
       if (jiraIssue && jiraIssue.id) {
+        console.log(`Fetched Jira issue ${jiraKey} with id ${jiraIssue.id}`);
         const commits = llmData.summary.commits;
 
         if (commits.length > 0) {
+          console.log(
+            `Fetched commits for branch ${branchName}:\n${JSON.stringify(commits)}`
+          );
           // Run summarizer
+          console.log('Feeding commits to LLM to summarize...');
           const summary = await summarizeFile(JSON.stringify(commits));
           if (summary) {
+            console.log(`Summary generated:\n${summary}`);
+            console.log(`Posting to Jira issue ${jiraKey}`);
             await addJiraComment(jiraKey, summary);
+            console.log('Successfuly posted summary!');
+          } else {
+            console.error('Error: No summary generated');
           }
+        } else {
+          console.warn('No commits found');
         }
+      } else {
+        console.warn('No Jira issue found');
       }
     } catch (error) {
       console.error('Error: ', error);
     }
+  } else {
+    console.error('No Jira key found');
   }
 }
